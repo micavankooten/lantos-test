f98e903317c55305dc01a10653a27fc777d18841
Gabu <gabriel.h.alegre@gmail.com>
2021-01-11 16:20:00 -0300

    rtop

---
+++ js/videos.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  var myPlayer = $(".videos").RTOP_VideoPlayer({
+    playInModal: false,
+    showCloseBtn: true,
+    showControls: true,
+    showControlsOnHover: true,
+
+    showScrubber: true,
+    showTimer: true,
+    showPlayPauseBtn: true,
+    showSoundControl: true,
+    showFullScreen: true,
+    keyboardControls: true,
+  });
+});
